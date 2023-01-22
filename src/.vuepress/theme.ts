92635411d7c63deb78108e6898ff033cd7696f8d
Lu Weizheng <luweizheng36@hotmail.com>
2023-01-22 23:38:34 +0800

    comment

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -85,28 +85,28 @@ export default hopeTheme({
     // If you don’t need comment feature, you can remove following option
     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-    // comment: {
-    //   /**
-    //    * Using Giscus
-    //    */
-    //   // provider: "Giscus",
-    //   // repo: "vuepress-theme-hope/giscus-discussions",
-    //   // repoId: "R_kgDOG_Pt2A",
-    //   // category: "Announcements",
-    //   // categoryId: "DIC_kwDOG_Pt2M4COD69",
+    comment: {
+      /**
+       * Using Giscus
+       */
+      provider: "Giscus",
+      repo: "luweizheng/lulaoshi.info",
+      repoId: "MDEwOlJlcG9zaXRvcnkyNTY2Mjg2Njk=",
+      category: "Announcements",
+      categoryId: "DIC_kwDOD0vXvc4CTvcP",
 
-    //   /**
-    //    * Using Twikoo
-    //    */
-    //   // provider: "Twikoo",
-    //   // envId: "https://twikoo.ccknbc.vercel.app",
+      /**
+       * Using Twikoo
+       */
+      // provider: "Twikoo",
+      // envId: "https://twikoo.ccknbc.vercel.app",
 
-    //   /**
-    //    * Using Waline
-    //    */
-    //   provider: "Waline",
-    //   serverURL: "https://vuepress-theme-hope-comment.vercel.app",
-    // },
+      /**
+       * Using Waline
+       */
+      // provider: "Waline",
+      // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
+    },
 
     // Disable features you don’t want here
     mdEnhance: {
